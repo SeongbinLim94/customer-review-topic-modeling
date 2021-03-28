663b2d15ecdf03a86011111512777671e14e9b6f
Seongbin Lim <lim.andrew1@gmail.com>
2021-03-28 18:05:50 +0900

    upload code and data

---
+++ lda-test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib import font_manager, rc
+import khaiii
+from khaiii import KhaiiiApi
+import seaborn as sns
+import re
+from gensim import corpora
+from gensim import models
+from gensim.models import LdaModel
+from gensim.models.coherencemodel import CoherenceModel
+import pyLDAvis
+import pyLDAvis.gensim
+import matplotlib.pyplot as plt
+%matplotlib inline
